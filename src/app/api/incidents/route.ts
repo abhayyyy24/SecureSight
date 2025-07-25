0500578baa1ad2aeb6f9fb28f0e12f312a52e773
abhayyyy24 <pawar2abhay1910@gmail.com>
2025-07-26 02:58:40 +0530

    incident error fix

--- src/app/api/incidents/route.ts
+++ src/app/api/incidents/route.ts
@@ -1,16 +1,20 @@
-import {prisma} from "../../../../lib/prisma"
-import { NextResponse } from "next/server"
-
-export async function GET(){
+import { prisma } from "../../../../lib/prisma";
+export async function GET() {
     try {
-        const incidents=await prisma.incident.findMany({
-            where: {resolved:false},
-            orderBy:{tsStart:'desc'},
-            include:{camera:true}
+        const incidents = await prisma.incident.findMany({
+            where: { resolved: false },
+            orderBy: { tsStart: 'desc' },
+            include: { camera: true }
         });
-        return NextResponse.json(incidents,{status:200});
-    }catch(error){
-        console.error("error  fetching Incidents",error);
-        return NextResponse.json({error:'Internal server error'},{status:500})
+        return new Response(JSON.stringify(incidents), {
+            status: 200,
+            headers: { "Content-Type": "application/json" }
+        });
+    } catch (error) {
+        console.error("error  fetching Incidents", error);
+        return new Response(JSON.stringify({ error: 'Internal server error' }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" }
+        });
     }
 }
